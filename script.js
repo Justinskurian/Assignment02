da684515156c2d8d71d2ec73a365ba99a5e4c98d
Justinskurian <justinskurian@gmail.com>
2024-08-30 00:14:29 +0530

    Form validation

--- script.js
+++ script.js
@@ -4,55 +4,40 @@ var err1=document.getElementById('err1');
 var err2=document.getElementById('err2');
 var nm=document.getElementById("exampleInputnumber");
 
-function validate(){
-    let isEmailValid = validateem();
-    let isPasswordValid = validatepwd();
-    let isPhoneValid = validatenm();
-    
-    return isEmailValid && isPasswordValid && isPhoneValid;
-}
 
-function validateem(){
-    let regEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if(regEx.test(em.value)){
-        err1.innerText="Valid Email";
-        err1.style.color="green";
-        return true;
-    }
-    if(em.value.trim()==""){
-        err1.innerText="E mail field cannot be empty";
-        return false;
-    }
-    else{
-        err1.innerText="Invalid Email";
+function validateem() {
+    if(em.value.trim() ==""){
+        err1.innerText = "Please fill this part";
         err1.style.color="red";
         return false;
-    }
+    } 
 }
 
-function validatepwd() {
-    let pwdregEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
-    if (pwdregEx.test(pwd.value)) {
-        return true;
-    }
-    if (pwd.value.trim() === "") {
-        err2.innerText = "Password field cannot be empty";
-        err2.style.color = "red";
+function validatepwd(){
+    if(pwd.value.trim()==""){
+        err2.innerText = "Please fill this part";
+        err2.style.color="red";
         return false;
-    } 
-    if (pwd.value.length >= 12) {
+    }
+    else if (pwd.value.length >= 12 ) {
         err2.innerText = "Strong";
         err2.style.color = "green";
         return true;
-    } else if (pwd.value.length >= 10) {
+    } 
+    else if (pwd.value.length == 10 || pwd.value.length == 11) {
         err2.innerText = "Medium";
         err2.style.color = "orange";
         return true;
-    } else if (pwd.value.length >= 8) {
+    } 
+    else if (pwd.value.length >= 8) {
         err2.innerText = "Weak";
         err2.style.color = "red";
         return true;
     }
+    else{
+        err2.innerText = "Check your password";
+        err2.style.color = "red";
+    } 
 }
 
 function validatenm(){
@@ -70,6 +55,19 @@ function validatenm(){
     else{
         err3.innerText="Inalid Phone Number";
         err3.style.color="red";
+        return false;
+    }
+}
+
+function validate(){
+    let regEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    let pwdregEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    let phonePattern = /^(?:\d{10}|\d{3}[-.\s]\d{3}[-.\s]\d{4})$/;
+    if (regEx.test(em.value) && pwdregEx.test(pwd.value) && phonePattern.test(nm.value)){
+        return true;
+    }
+    else{
+        alert("Fill the form correctly");
         return false;
     }
 }
